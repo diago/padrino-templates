5635c399de326b06dd4675ee7fedd0feb3f35cf5
diago <diago@seaofmess.net>
2011-04-05 16:17:28 -0400

    updated: standard template with git commands

--- standard_template.rb
+++ standard_template.rb
@@ -9,6 +9,7 @@ project :adapter    => :sqlite,
         :script     => :jquery,
         :stylesheet => :compass
 
+say "Adding test gems"
 TEST_GEMS = <<-GEM
 gem 'database_cleaner', :group => "test"
 gem 'factory_girl', :group => "test"
@@ -16,6 +17,7 @@ gem 'spork', '~> 0.9.0rc', :group => "test"
 GEM
 inject_into_file destination_root('Gemfile'), TEST_GEMS, :after => "# Test requirements\n"
 
+say "Fixing YAML issue"
 YAML_FIX = <<-FIX
    YAML::ENGINE.yamler= 'syck'
 FIX
@@ -24,7 +26,27 @@ inject_into_file destination_root('config/boot.rb'), YAML_FIX, :after => "Padrin
 run_bundler
 
 # Overwrite features/support/env.rb
-get 'https://github.com/diago/padrino-templates/raw/master/features/support/env.rb', 'features/support/env.rb'
+say "=> Overwriting cucumber env.rb"
+file_env = 'features/support/env.rb'
+remove_file(file_env)
+get 'https://github.com/diago/padrino-templates/raw/master/features/support/env.rb', file_env
 
 # Overwrite spec/spec_helper.rb
-get 'https://github.com/diago/padrino-templates/raw/master/spec/spec_helper.rb', 'spec/spec_helper.rb'
+say "=> Overwriting rspec spec_helper.rb"
+file_spec_helper = 'spec/spec_helper.rb'
+remove_file(file_spec_helper)
+get 'https://github.com/diago/padrino-templates/raw/master/spec/spec_helper.rb', file_spec_helper
+
+
+# Git SCM
+git :init
+
+# say
+# if no?('Use your global git configuration?')
+#   user_name  = ask('Git user.name:')
+#   user_email = ask('Git user.email')
+#   @TODO figure out how to set configs
+# end
+
+git :add, '.'
+git :commit, 'init commit'
